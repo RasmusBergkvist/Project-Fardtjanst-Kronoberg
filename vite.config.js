16a1d9f2ab88f250c754991353cc7c142d4e116f
Rasmus Bergkvist <bergqvist.rasmus@gmail.com>
2026-03-17 21:00:10 +0100

    First commit

---
+++ vite.config.js
@@ -0,0 +1,26 @@
+import { defineConfig } from "vite";
+import { resolve } from "path";
+
+export default defineConfig({
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, "index.html"),
+        boka: resolve(__dirname, "boka-resa.html"),
+        bokadresa: resolve(__dirname, "boka-resa.html"),
+        minabokningar: resolve(__dirname, "mina-bokningar.html"),
+        faq: resolve(__dirname, "vanliga-fragor.html"),
+        kontakt: resolve(__dirname, "kontakta-oss.html"),
+        minasidor: resolve(__dirname, "mina-sidor.html"),
+        omboka: resolve(__dirname, "omboka-resa.html"),
+        ombokadresa: resolve(__dirname, "ombokad-resa.html"),
+        avboka: resolve(__dirname, "avboka-resa.html"),
+        avbokadresa: resolve(__dirname, "avbokad-resa.html"),
+        tillganglighetsredogorelse: resolve(__dirname, "tillganglighetsredogorelse.html")
+      }
+    }
+  },
+  css: {
+    devSourcemap: true
+  }
+});
