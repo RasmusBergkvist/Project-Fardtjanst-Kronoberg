5c067bc928caed20d3ac40b428cad5fbdcd8b3af
Rasmus Bergkvist <bergqvist.rasmus@gmail.com>
2026-03-24 11:53:03 +0100

    Lagt till hamburger-meny med toogle för att öppna och stänga menyn.

--- src/main.js
+++ src/main.js
@@ -1,3 +1,28 @@
+"use strict";
+
 import './scss/main.scss'
 
+document.addEventListener('DOMContentLoaded', () => {
 
+
+    const menuButton = document.getElementById('mobile-menu');
+    const menu = document.getElementById('menu');
+
+
+    menuButton.addEventListener('click', () => {
+        menu.classList.toggle('active')
+
+        const isOpen =  menu.classList.contains('active');
+
+        if (isOpen) {
+            menuButton.innerHTML = '✕';
+            menuButton.setAttribute('aria-label', 'Stäng menyn');
+        } else {
+            menuButton.innerHTML = '&#9776;';
+            menuButton.setAttribute('aria-label', 'Öppna menyn');
+        }
+
+    });
+
+
+});
