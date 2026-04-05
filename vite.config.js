f12f2bb97192587e225cc0efaa7cf336633d73e0
Rasmus Bergkvist <bergqvist.rasmus@gmail.com>
2026-04-05 10:33:11 +0200

    merge till main och uppdaterat länkar.

--- vite.config.js
+++ vite.config.js
@@ -8,16 +8,16 @@ export default defineConfig({
         main: resolve(__dirname, "index.html"),
         boka: resolve(__dirname, "boka-resa.html"),
         bokadresa: resolve(__dirname, "bokad-resa.html"),
-        minabokningar: resolve(__dirname, "mina-bokningar.html"),
+        minabokaderesor: resolve(__dirname, "mina-bokade-resor.html"),
         faq: resolve(__dirname, "vanliga-fragor.html"),
-        kontakt: resolve(__dirname, "kontakta-oss.html"),
+        kontaktaoss: resolve(__dirname, "kontakta-oss.html"),
         minasidor: resolve(__dirname, "mina-sidor.html"),
         omboka: resolve(__dirname, "omboka-resa.html"),
         ombokadresa: resolve(__dirname, "ombokad-resa.html"),
         avbokadresa: resolve(__dirname, "avbokad-resa.html"),
         tillganglighetsredogorelse: resolve(__dirname, "tillganglighetsredogorelse.html"),
-        synpunkter: resolve(__dirname, "synpunkter-tillgangliget.html"),
-        logout: resolve(__dirname, "loggaut.html")
+        synpunktertillganglighet: resolve(__dirname, "synpunkter-tillganglighet.html"),
+        loggaut: resolve(__dirname, "loggaut.html")
       }
     }
   },
